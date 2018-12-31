0506b4343060a246a9d086cbf1b3ecea908cffa4
Robert Sepsi <sepsi.robert@gmail.com>
2018-12-31 16:48:10 +0100

    added some code cleanup, polishing prints

--- oco_order.py
+++ oco_order.py
@@ -1,7 +1,5 @@
 from binance.client import Client
 from api_keys import api_key_1
-import pandas as pd
-
 import time
 import win32api
 
@@ -16,17 +14,22 @@ ASSET = 'BNB'
 TARGETPRICE_1 = 0.001753
 
 # Define code parameters
-COUNTERMAX = 2
+COUNTERMAX = 20
 
-# Create client for exchange package
+
+# Starting the script
+
+
+# Create client connection to binance by using the User keys
 client = Client(api_key, api_secret)
+
+# Following few lines is to sync system time to binance server time - needed to bypass binance API security for synced times
 gt = client.get_server_time()
 tt=time.gmtime(int((gt["serverTime"])/1000))
 win32api.SetSystemTime(tt[0],tt[1],0,tt[2],tt[3],tt[4],tt[5],0)
 
-# Create a counter to control infinite while loops
-counter = 0
-
+# Requesting ASSET balance and priting them
+print("--- Binance "+ASSET+" ---")
 balance = client.get_asset_balance(asset = ASSET)
 free_balance = balance['free']
 locked_balance = balance['locked']
@@ -35,22 +38,37 @@ print("Free balance: "+str(free_balance))
 print("Locked balance: "+str(locked_balance))
 print("Total balance: "+str(total_balance))
 
+print("--- Starting price monitoring ---")
+
 # Run a continuous loop to check on price and execute (something)
 #while total_balance != 0:
-while 1:
+
+# Create a counter to control infinite while loops
+counter = 0
+
+while counter < COUNTERMAX:
+
+    counter += 1
+    print("...#"+str(counter))
 
     # Get all prices using exchange package
-    prices = client.get_all_tickers()
+    prices_binanceclient = client.get_all_tickers()
 
-    # Select price of the specific pair
-    price = pd.DataFrame(prices)
-    price = float(price[price['symbol'] == SYMBOL]['price'])
+    # Defining an empty dict for prices
+    prices={}
 
-    print(price)
+    # Converting binance response into the prices dict
+    for asset in prices_binanceclient:
+        prices[asset['symbol']]=asset['price']
+
+    #price = pd.DataFrame(prices)
+    price = float(prices[SYMBOL])
+
+    msg = "current price: "+str(price)
 
     # If price reaches target, place market sell order
     if price >= TARGETPRICE_1 :
-        print('SELL!')
+        print(msg+'-> SELL!')
         # Currently limit buy for test purposes
         # order = client.order_limit_buy(
         #     symbol = symbol,
@@ -58,9 +76,6 @@ while 1:
         #     price = '0.000100'
         # )
     else :
-        print('HODL!')
+        print(msg+'-> HODL!')
 
-    counter += 1
-    print("counter = "+str(counter))
-    if counter > COUNTERMAX:
-        break
+
