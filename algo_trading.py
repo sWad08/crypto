da612eeae2585028ccbe7c7a9d0b6882c18c01f4
sWad08 <horvath.gergo1988@gmail.com>
2019-03-10 11:32:10 +0100

    Prepared code for iterating over signals DataFrame

--- algo_trading.py
+++ algo_trading.py
@@ -45,45 +45,48 @@ initial_capital = float(3000.0)
 
 # Initialize a DataFrame for signals
 signals = pd.DataFrame(index=inputs.index)
-signals['signal'] = 0.0
+
+# Load open and close prices from inputs
+signals['asset_price_open'] = inputs['Open']
+signals['asset_price_close'] = inputs['Close']
 
 # Calculate moving averages based on windows defined earlier
-signals['ma_short'] = inputs['Close'].rolling(window=window_short, min_periods=1, center=False).mean()
-signals['ma_long'] = inputs['Close'].rolling(window=window_long, min_periods=1, center=False).mean()
+signals['ma_short'] = signals['asset_price_close'].rolling(window=window_short, min_periods=1, center=False).mean()
+signals['ma_long'] = signals['asset_price_close'].rolling(window=window_long, min_periods=1, center=False).mean()
 
 # Create signals: 1 if bullish, 0 if bearish
+signals['signal'] = 0.0  # Preload with zeroes
 signals['signal'][window_short:] = np.where(signals['ma_short'][window_short:] > signals['ma_long'][window_short:], 1.0, 0.0)  # Only for the period greater than the short MA -- IS THAT CORRECT? SHOULDN'T IT BE THE LONG MA?
 
-# Generate triggers (change in signals along time): go long if 1, go short if -1
-signals['trigger'] = signals['signal'].diff()
-
-# Initialize a DataFrame to keep track of portfolio
-portfolio = pd.DataFrame(index=signals.index).fillna(0.0)
-
-# Simulate owning x units of asset when signal is bullish -- WE SHOULD DEFINE QUANTITY RELATIVE TO PORTFOLIO VALUE BUT THAT WOULD REQUIRE SWITCHING TO A LOOPING LOGIC
-x = 0.56
-portfolio['asset_qty'] = x * signals['signal']
-
-# Calculate the change in asset quantity along time
-portfolio['asset_qty_chg'] = portfolio['asset_qty'].diff()
-
-# Calculate asset value, cash value and total portfolio value
-portfolio['asset_value'] = portfolio['asset_qty'].multiply(inputs['Close'], axis=0)
-portfolio['cash_value'] = initial_capital - (portfolio['asset_qty_chg'].multiply(inputs['Close'], axis=0)).cumsum()
-portfolio['portf_value'] = portfolio['asset_value'] + portfolio['cash_value']
+# Generate triggers if the signal changed in the previous period: go long if 1, go short if -1
+signals['trigger'] = 0.0  # Preload with zeroes
+signals['trigger'] = signals['signal'].diff().shift(1)
 
 # Calculate asset quantity for benchmark: buying and holding as many units of asset as the initial capital enables
-initial_price = inputs['Close'].iloc[0]
+initial_price = signals['asset_price_open'].iloc[0]
 bm_qty = initial_capital / initial_price
 
 # Add benchmark quantity and value to portfolio
-portfolio['bm_qty'] = bm_qty
-portfolio['bm_value'] = portfolio['bm_qty'].multiply(inputs['Close'], axis=0)
+signals['bm_qty'] = bm_qty
+signals['bm_value'] = signals['bm_qty'].multiply(signals['asset_price_open'])
 
-print("Portfolio value: " + str(portfolio['portf_value'].iloc[-1]))
-print("Benchmark value: " + str(portfolio['bm_value'].iloc[-1]))
-print("Minimum cash value: " + str(min(portfolio['cash_value'][1:])))  # Check if cash value at any point turned negative
+# Preload columns necessary for portfolio value calculation
+signals['order_qty'] = 0
+signals['asset_qty_open'] = 0
+signals['asset_qty_close'] = 0
+signals['asset_value_open'] = 0
+signals['asset_value_close'] = 0
+signals['cash_value_open'] = initial_capital
+signals['cash_value_close'] = initial_capital
+signals['portf_value_open'] = initial_capital
+signals['portf_value_close'] = initial_capital
 
-# portfolio.to_csv('portfolio.csv')
+# NEXT STEP: iterate over specific parts of the DataFrame to carry out specific calculations
+# (e.g.: open values are equal to the close values of the previous period)
 
+print("Portfolio value: " + str(signals['portf_value_close'].iloc[-1]))
+print("Benchmark value: " + str(signals['bm_value'].iloc[-1]))
 
+print(signals.head())
+
+# signals.to_csv('portfolio.csv')
