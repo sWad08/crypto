6861e3cb3b60bcd94c26522728fc2a879fc2f789
Sepsi Robi <sepsi.robert@gmail.com>
2019-03-12 00:45:11 +0100

    improved performance by less dataframe .loc operations AND by decreasing initial dataframe size according to end limiter parameter
    also changed constants into capital letters

--- algo_trading.py
+++ algo_trading.py
@@ -5,25 +5,31 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+
 reinv_ratio = 1.0
-
 trading_fee = 0.001
-
 end = 100
 
-trade_price = 'trade_price'
-trade_qty = 'trade_qty'
-asset_qty_open = 'asset_qty_open'
-asset_qty_close = 'asset_qty_close'
-asset_value_open = 'asset_value_open'
-asset_value_close = 'asset_value_close'
-cash_value_open = 'cash_value_open'
-cash_value_close = 'cash_value_close'
-portf_value_open = 'portf_value_open'
-portf_value_close = 'portf_value_close'
+# Define short and long windows for moving average calculation
+window_short = 13
+window_long = 44
 
-asset_price_open = 'asset_price_open'
-asset_price_close = 'asset_price_close'
+# Set initial capital for backtesting
+initial_capital = float(3000.0)
+
+TRIGGER = 'trigger'
+TRADE_PRICE = 'trade_price'
+TRADE_QTY = 'trade_qty'
+ASSET_QTY_OPEN = 'asset_qty_open'
+ASSET_QTY_CLOSE = 'asset_qty_close'
+ASSET_VALUE_OPEN = 'asset_value_open'
+ASSET_VALUE_CLOSE = 'asset_value_close'
+CASH_VALUE_OPEN = 'cash_value_open'
+CASH_VALUE_CLOSE = 'cash_value_close'
+PORTF_VALUE_OPEN = 'portf_value_open'
+PORTF_VALUE_CLOSE = 'portf_value_close'
+ASSET_PRICE_OPEN = 'asset_price_open'
+ASSET_PRICE_CLOSE = 'asset_price_close'
 
 
 # Define API key and secret as variables
@@ -60,117 +66,106 @@ inputs = pd.read_csv('klines.csv', index_col=0)
 inputs['Open time'] = pd.to_datetime(inputs['Open time'], unit='ms')
 inputs.set_index('Open time', inplace=True)
 
-# Define short and long windows for moving average calculation
-window_short = 13
-window_long = 44
-
-# Set initial capital for backtesting
-initial_capital = float(3000.0)
+inputs = inputs.iloc[0:end]
 
 # Initialize a DataFrame for signals
 signals = pd.DataFrame(index=inputs.index)
 
 # Load open and close prices from inputs
-signals['asset_price_open'] = inputs['Open']
-signals['asset_price_close'] = inputs['Close']
+signals[ASSET_PRICE_OPEN] = inputs['Open']
+signals[ASSET_PRICE_CLOSE] = inputs['Close']
 
 # Calculate moving averages based on windows defined earlier
-signals['ma_short'] = signals['asset_price_close'].rolling(window=window_short, min_periods=1, center=False).mean()
-signals['ma_long'] = signals['asset_price_close'].rolling(window=window_long, min_periods=1, center=False).mean()
+signals['ma_short'] = signals[ASSET_PRICE_CLOSE].rolling(window=window_short, min_periods=1, center=False).mean()
+signals['ma_long'] = signals[ASSET_PRICE_CLOSE].rolling(window=window_long, min_periods=1, center=False).mean()
 
 # Create signals: 1 if bullish, 0 if bearish
 signals['signal'] = 0.0  # Preload with zeroes
 signals['signal'][window_short:] = np.where(signals['ma_short'][window_short:] > signals['ma_long'][window_short:], 1.0, 0.0)  # Only for the period greater than the short MA -- IS THAT CORRECT? SHOULDN'T IT BE THE LONG MA?
 
 # Generate triggers if the signal changed in the previous period: go long if 1, go short if -1
-signals['trigger'] = 0.0  # Preload with zeroes
-signals['trigger'] = signals['signal'].diff().shift(1)
+signals[TRIGGER] = 0.0  # Preload with zeroes
+signals[TRIGGER] = signals['signal'].diff().shift(1)
 
 # Calculate asset quantity for benchmark: buying and holding as many units of asset as the initial capital enables
-initial_price = signals['asset_price_open'].iloc[0]
+initial_price = signals[ASSET_PRICE_OPEN].iloc[0]
 bm_qty = initial_capital / initial_price
 
 # Add benchmark quantity and value to portfolio
 signals['bm_qty'] = bm_qty
-signals['bm_value'] = signals['bm_qty'].multiply(signals['asset_price_open'])
+signals['bm_value'] = signals['bm_qty'].multiply(signals[ASSET_PRICE_OPEN])
 
 # Preload columns necessary for portfolio value calculation
-signals['trade_price'] = signals['asset_price_open']
-signals['trade_qty'] = 0.0
-signals['asset_qty_open'] = 0.0
-signals['asset_qty_close'] = 0.0
-signals['asset_value_open'] = 0.0
-signals['asset_value_close'] = 0.0
-signals['cash_value_open'] = 0.0
-signals['cash_value_close'] = 0.0
-signals['portf_value_open'] = 0.0
-signals['portf_value_close'] = 0.0
+signals[TRADE_PRICE] = signals[ASSET_PRICE_OPEN]
+signals[TRADE_QTY] = 0.0
+signals[ASSET_QTY_OPEN] = 0.0
+signals[ASSET_QTY_CLOSE] = 0.0
+signals[ASSET_VALUE_OPEN] = 0.0
+signals[ASSET_VALUE_CLOSE] = 0.0
+signals[CASH_VALUE_OPEN] = 0.0
+signals[CASH_VALUE_CLOSE] = 0.0
+signals[PORTF_VALUE_OPEN] = 0.0
+signals[PORTF_VALUE_CLOSE] = 0.0
 
-signals['cash_value_open'][0] = initial_capital
-signals['cash_value_close'][0] = initial_capital
-signals['portf_value_open'][0] = initial_capital
-signals['portf_value_close'][0] = initial_capital
+signals[CASH_VALUE_OPEN][0] = initial_capital
+signals[CASH_VALUE_CLOSE][0] = initial_capital
+signals[PORTF_VALUE_OPEN][0] = initial_capital
+signals[PORTF_VALUE_CLOSE][0] = initial_capital
 
 # NEXT STEP: iterate over specific parts of the DataFrame to carry out specific calculations
 # (e.g.: open values are equal to the close values of the previous period)
-open_list = [asset_qty_open, asset_value_open, cash_value_open, portf_value_open]
-close_list = [asset_qty_close, asset_value_close, cash_value_close, portf_value_close]
+open_list = [ASSET_QTY_OPEN, ASSET_VALUE_OPEN, CASH_VALUE_OPEN, PORTF_VALUE_OPEN]
+close_list = [ASSET_QTY_CLOSE, ASSET_VALUE_CLOSE, CASH_VALUE_CLOSE, PORTF_VALUE_CLOSE]
 
 
 counter = 0
-for period in signals.index:
+for period in signals.index[1:]:
 
-    if period != signals.index[0]:
-        counter += 1
-        #signals.loc[[period], open_list] = signals.shift(1).loc[[period], close_list]
-        signals.loc[[period], asset_qty_open] = signals.shift(1).loc[[period], asset_qty_close]
-        signals.loc[[period], asset_value_open] = signals.shift(1).loc[[period], asset_value_close]
-        signals.loc[[period], cash_value_open] = signals.shift(1).loc[[period], cash_value_close]
-        signals.loc[[period], portf_value_open] = signals.shift(1).loc[[period], portf_value_close]
+    counter += 1
 
-        cash_open = signals.loc[[period], cash_value_open][0]
-        asset_pr_open = signals.loc[[period], asset_price_open][0]
-        asset_pr_close = signals.loc[[period], asset_price_close][0]
-        asset_quantity_open = signals.loc[[period], asset_qty_open][0]
+    # value read (and assigning to variables) from sub-DataFrame
+    # by converting the numpy array from .values[0] into a list
+    asset_qty_close, asset_value_close, cash_value_close, portf_value_close = list(signals.shift(1).loc[[period], close_list].values[0])
 
-        trigger = signals.loc[[period], 'trigger'][0]
-        trade_pr = asset_pr_open
+    asset_qty_open, asset_value_open, cash_value_open, portf_value_open = asset_qty_close, asset_value_close, cash_value_close, portf_value_close
 
-        if np.isnan(trigger):
-            trade_quantity = 0.0
-        elif trigger == 1.0:
-            trade_quantity = reinv_ratio * cash_open / trade_pr * (1.0 - trading_fee)
-        elif trigger == -1.0:
-            trade_quantity = asset_quantity_open * trigger
-        else:
-            trade_quantity = 0.0
+    asset_price_open, asset_price_close, trigger = \
+        list(signals.loc[[period], [ASSET_PRICE_OPEN, ASSET_PRICE_CLOSE, 'trigger']].values[0])
 
-        asset_quantity_close = asset_quantity_open + trade_quantity
+    trade_price = asset_price_open
 
-        asset_close = asset_quantity_close * asset_pr_close
+    if np.isnan(trigger):
+        trade_qty = 0.0
+    elif trigger == 1.0:
+        trade_qty = reinv_ratio * cash_value_open / trade_price * (1.0 - trading_fee)
+    elif trigger == -1.0:
+        trade_qty = asset_qty_open * trigger
+    else:
+        trade_qty = 0.0
 
-        cash_close = cash_open - (trade_quantity * trade_pr / (1.0 - trading_fee))
+    asset_qty_close = asset_qty_open + trade_qty
 
-        portf_close = asset_close + cash_close
+    asset_value_close = asset_qty_close * asset_price_close
 
-        signals.loc[[period], trade_qty] = trade_quantity
-        signals.loc[[period], asset_qty_close] = asset_quantity_close
-        signals.loc[[period], asset_value_close] = asset_close
-        signals.loc[[period], cash_value_close] = cash_close
-        signals.loc[[period], portf_value_close] = portf_close
+    cash_value_close = cash_value_open - (trade_qty * trade_price / (1.0 - trading_fee))
 
+    portf_value_close = asset_value_close + cash_value_close
+
+    # value assignment into DataFrame (.loc is a sub dataframe) via np.array (from a list)
+    signals.loc[[period], close_list + [TRADE_QTY]] = \
+        np.array([asset_qty_close, asset_value_close, cash_value_close, portf_value_close, trade_qty])
+
+    if counter % 100 == 0:
         print("did " + str(counter))
-        if counter == end:
-            break
 
 
 print("Portfolio value: " + str(signals['portf_value_close'].iloc[counter-1]))
 print("Benchmark value: " + str(signals['bm_value'].iloc[counter-1]))
 
-print(signals.iloc[0:counter].tail())
+print(signals.tail())
 
-plt.plot(signals.iloc[0:counter].index, signals.iloc[0:counter].portf_value_close,color='black',label='PortfValue')
-plt.plot(signals.iloc[0:counter].index, signals.iloc[0:counter].bm_value,color='red',label='BenchmValue')
+plt.plot(signals.index, signals.portf_value_close,color='black',label='PortfValue')
+plt.plot(signals.index, signals.bm_value,color='red',label='BenchmValue')
 plt.xlabel('Time')
 plt.ylabel('Dollar')
 plt.legend()
