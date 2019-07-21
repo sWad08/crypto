52ab2d718a862d9da4064c99b36b8d30d190fdbd
sWad <horvath.gergo1988@gmail.com>
2019-07-21 19:58:23 +0200

    Created logic to backtest different combinations of moving averages

--- algo_trading.py
+++ algo_trading.py
@@ -3,6 +3,8 @@ from api_keys import api_key_1
 import pandas as pd
 import numpy as np
 
+from calcs import moving_avg_signal
+
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -11,13 +13,27 @@ reinv_ratio = 0.98
 trade_fee_rate = 0.00075
 end = 15000
 
+window_list = [1,2,5,8,13,21,34,55]
+
+window_short = []
+window_long = []
+
+for short in window_list:
+    for long in window_list:
+        if short < long:
+            window_short.append(short)
+            window_long.append(long)
+
+print(window_short)
+print(window_long)
+print()
+
 # Define short and long windows for moving average calculation
-window_short = 13
-window_long = 44
+# window_short = [10]
+# window_long =  [20]
 
 # Define start date for backtesting
 start_date = pd.Timestamp('2019-01-01')
-
 
 # Set initial capital for backtesting
 initial_capital = float(3000.0)
@@ -39,7 +55,6 @@ PORTF_VALUE_OPEN = 'portf_value_open'
 PORTF_VALUE_CLOSE = 'portf_value_close'
 ASSET_PRICE_OPEN = 'asset_price_open'
 ASSET_PRICE_CLOSE = 'asset_price_close'
-
 
 # Define API key and secret as variables
 api_key = api_key_1['api_key']
@@ -85,17 +100,10 @@ signals = pd.DataFrame(index=inputs.index)
 signals[ASSET_PRICE_OPEN] = inputs['Open']
 signals[ASSET_PRICE_CLOSE] = inputs['Close']
 
-# Calculate moving averages based on windows defined earlier
-signals['ma_short'] = signals[ASSET_PRICE_CLOSE].rolling(window=window_short, min_periods=1, center=False).mean()
-signals['ma_long'] = signals[ASSET_PRICE_CLOSE].rolling(window=window_long, min_periods=1, center=False).mean()
+for i in range(len(window_short)):
 
-# Create signals: 1 if bullish, 0 if bearish
-signals['signal'] = 0.0  # Preload with zeroes
-signals['signal'][window_short:] = np.where(signals['ma_short'][window_short:] > signals['ma_long'][window_short:], 1.0, 0.0)  # Only for the period greater than the short MA -- IS THAT CORRECT? SHOULDN'T IT BE THE LONG MA?
-
-# Generate triggers if the signal changed in the previous period: go long if 1, go short if -1
-signals[TRIGGER] = 0.0  # Preload with zeroes
-signals[TRIGGER] = signals['signal'].diff().shift(1)
+    # Calculate moving averages based on windows defined earlier
+    moving_avg_signal(signals,window_short[i],window_long[i],col_name_override=TRIGGER + '_' + str(i))
 
 # Calculate asset quantity for benchmark: buying and holding as many units of asset as the initial capital enables
 initial_price = signals[ASSET_PRICE_OPEN].at[start_date]
@@ -109,32 +117,45 @@ signals['bm_qty'] = bm_qty
 signals['bm_value_open'] = signals['bm_qty'].multiply(signals[ASSET_PRICE_OPEN])
 signals['bm_value_close'] = signals['bm_qty'].multiply(signals[ASSET_PRICE_CLOSE])
 
-# Preload columns necessary for portfolio value calculation
-signals[TRADE_PRICE] = signals[ASSET_PRICE_OPEN]
-signals[REINV_VALUE] = 0.0
-signals[TRADE_QTY] = 0.0
-signals[TRADE_VALUE_NET] = 0.0
-signals[TRADE_FEE] = 0.0
-signals[TRADE_VALUE_GROSS] = 0.0
-signals[ASSET_QTY_OPEN] = 0.0
-signals[ASSET_QTY_CLOSE] = 0.0
-signals[ASSET_VALUE_OPEN] = 0.0
-signals[ASSET_VALUE_CLOSE] = 0.0
-signals[CASH_VALUE_OPEN] = 0.0
-signals[CASH_VALUE_CLOSE] = 0.0
-signals[PORTF_VALUE_OPEN] = 0.0
-signals[PORTF_VALUE_CLOSE] = 0.0
+signals_list = list()
+multiindex_df = pd.DataFrame()
 
-signals[CASH_VALUE_OPEN][0] = initial_capital
-signals[CASH_VALUE_CLOSE][0] = initial_capital
-signals[PORTF_VALUE_OPEN][0] = initial_capital
-signals[PORTF_VALUE_CLOSE][0] = initial_capital
+for i in range(len(window_short)):
+    signal_df = pd.DataFrame()
+    signal_df['bm_value_close'] = signals['bm_value_close']
+    signal_df[ASSET_PRICE_OPEN] = signals[ASSET_PRICE_OPEN]
+    signal_df[ASSET_PRICE_CLOSE] = signals[ASSET_PRICE_CLOSE]
+    signal_df[TRIGGER] = signals[TRIGGER + '_' + str(i)]
+    # Preload columns necessary for portfolio value calculation
+    signal_df[TRADE_PRICE] = signal_df[ASSET_PRICE_OPEN]
+    signal_df[REINV_VALUE] = 0.0
+    signal_df[TRADE_QTY] = 0.0
+    signal_df[TRADE_VALUE_NET] = 0.0
+    signal_df[TRADE_FEE] = 0.0
+    signal_df[TRADE_VALUE_GROSS] = 0.0
+    signal_df[ASSET_QTY_OPEN] = 0.0
+    signal_df[ASSET_QTY_CLOSE] = 0.0
+    signal_df[ASSET_VALUE_OPEN] = 0.0
+    signal_df[ASSET_VALUE_CLOSE] = 0.0
+    signal_df[CASH_VALUE_OPEN] = 0.0
+    signal_df[CASH_VALUE_CLOSE] = 0.0
+    signal_df[PORTF_VALUE_OPEN] = 0.0
+    signal_df[PORTF_VALUE_CLOSE] = 0.0
+
+    signal_df[CASH_VALUE_OPEN][0] = initial_capital
+    signal_df[CASH_VALUE_CLOSE][0] = initial_capital
+    signal_df[PORTF_VALUE_OPEN][0] = initial_capital
+    signal_df[PORTF_VALUE_CLOSE][0] = initial_capital
+
+    signals_list.append(signal_df)
+
+signals_names = tuple(['signals_' + str(x) for x in range(len(signals_list))])
+multiindex_df = pd.concat(signals_list, keys=signals_names)
 
 # NEXT STEP: iterate over specific parts of the DataFrame to carry out specific calculations
 # (e.g.: open values are equal to the close values of the previous period)
 open_list = [ASSET_QTY_OPEN, ASSET_VALUE_OPEN, CASH_VALUE_OPEN, PORTF_VALUE_OPEN]
 close_list = [ASSET_QTY_CLOSE, ASSET_VALUE_CLOSE, CASH_VALUE_CLOSE, PORTF_VALUE_CLOSE]
-
 
 counter = 0
 for period in signals.index[1:]:
@@ -143,22 +164,25 @@ for period in signals.index[1:]:
 
     # Read values from sub-DataFrame (and assign them to variables)
     # by converting the numpy array from .values[0] into a list
-    asset_qty_open, asset_value_open, cash_value_open, portf_value_open = list(signals.shift(1).loc[[period], close_list].values[0])
+    signal_period_pairs = [tuple([x,period]) for x in signals_names]
+    asset_qty_open, asset_value_open,cash_value_open, portf_value_open = list(multiindex_df.shift(1).loc[signal_period_pairs,close_list].values.T)
 
     asset_price_open, asset_price_close, trigger = \
-        list(signals.loc[[period], [ASSET_PRICE_OPEN, ASSET_PRICE_CLOSE, 'trigger']].values[0])
+        list(multiindex_df.loc[signal_period_pairs, [ASSET_PRICE_OPEN, ASSET_PRICE_CLOSE, 'trigger']].values.T)
 
     trade_price = asset_price_open
     reinv_value = reinv_ratio * cash_value_open
 
-    if np.isnan(trigger):
-        trade_qty = 0.0
-    elif trigger == 1.0:
-        trade_qty = reinv_value / trade_price
-    elif trigger == -1.0:
-        trade_qty = asset_qty_open * trigger
-    else:
-        trade_qty = 0.0
+    trade_qty = np.ndarray(len(trigger))
+    for i in range(len(trigger)):
+        if np.isnan(trigger[i]):
+            trade_qty[i] = 0.0
+        elif trigger[i] == 1.0:
+            trade_qty[i] = reinv_value[i] / trade_price[i]
+        elif trigger[i] == -1.0:
+            trade_qty[i] = asset_qty_open[i] * trigger[i]
+        else:
+            trade_qty[i] = 0.0
 
     trade_value_net = trade_price * trade_qty
     trade_fee = trade_fee_rate * trade_value_net
@@ -169,22 +193,36 @@ for period in signals.index[1:]:
     portf_value_close = asset_value_close + cash_value_close
 
     # Assign values into DataFrame (.loc is a sub DataFrame) via np.array (from a list)
-    signals.loc[[period], open_list + close_list + [REINV_VALUE] + [TRADE_QTY] + [TRADE_VALUE_NET] + [TRADE_FEE] + [TRADE_VALUE_GROSS]] = \
+    multiindex_df.loc[signal_period_pairs, open_list + close_list + [REINV_VALUE] + [TRADE_QTY] + [TRADE_VALUE_NET] + [TRADE_FEE] + [TRADE_VALUE_GROSS]] = \
         np.array([asset_qty_open, asset_value_open, cash_value_open, portf_value_open,
                   asset_qty_close, asset_value_close, cash_value_close, portf_value_close,
-                  reinv_value, trade_qty, trade_value_net, trade_fee, trade_value_gross])
+                  reinv_value, trade_qty, trade_value_net, trade_fee, trade_value_gross]).T
 
     if counter % 100 == 0:
-        print("Processed " + str(counter) + " rows")
+        print("Processed " + str(counter) + " periods")
+try:
+    multiindex_df.to_csv('signals.csv')
+except:
+    print("Can't use signals.csv file for saving...")
 
-signals.to_csv('signals.csv')
+top_3_idx = np.argsort(portf_value_close)[-3:]
 
-print("Portfolio value: " + str(signals['portf_value_close'].iloc[counter-1]))
-print("Benchmark value: " + str(signals['bm_value_close'].iloc[counter-1]))
+print("\nBenchmark value: " + str(signals['bm_value_close'].iloc[counter-1]))
+# print("Portfolio value: " + str(signals['portf_value_close'].iloc[counter-1]))
 
-print(signals.describe())
+colors = ['r', 'g', 'b']
 
-plt.plot(signals.index, signals.portf_value_close, color='blue', label='Portfolio value')
+for i in range(len(top_3_idx)):
+
+    idx = top_3_idx[i]
+
+    print("Portfolio value MA(" + str(window_short[idx]) + "," + str(window_long[idx]) + "): " + str(multiindex_df.loc['signals_' + str(idx),].portf_value_close[-1]))
+
+    plt.plot(multiindex_df.loc['signals_' + str(idx),].index,
+             multiindex_df.loc['signals_' + str(idx),].portf_value_close,
+             color = colors[i],
+             label="Portfolio value MA(" + str(window_short[idx]) + "," + str(window_long[idx]) + ")")
+
 plt.plot(signals.index, signals.bm_value_close, color='black', label='Benchmark value')
 plt.xlabel('Time')
 plt.ylabel('USD')
---
+++ calcs.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+def moving_avg_signal(df,window_short,window_long,col_name_override=None):
+
+    if 'asset_price_close' in df.columns:
+        # Calculate moving averages based on windows defined earlier
+        df['ma_short'] = df['asset_price_close'].rolling(window=window_short, min_periods=1, center=False).mean()
+        df['ma_long'] = df['asset_price_close'].rolling(window=window_long, min_periods=1, center=False).mean()
+
+        # Create signals: 1 if bullish, 0 if bearish
+        df['signal'] = 0.0  # Preload with zeroes
+        df['signal'][window_short:] = np.where(df['ma_short'][window_short:] > df['ma_long'][window_short:], 1.0, 0.0)  # Only for the period greater than the short MA -- IS THAT CORRECT? SHOULDN'T IT BE THE LONG MA?
+
+        col_name = 'TRIGGER_' + str(window_short) + '_' + str(window_long) if col_name_override is None else col_name_override
+
+        # Generate triggers if the signal changed in the previous period: go long if 1, go short if -1
+        df[col_name] = 0.0  # Preload with zeroes
+        df[col_name] = df['signal'].diff().shift(1)
+
